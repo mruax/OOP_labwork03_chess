e9822d907ea4b1e648e2bfe750c380d4120784d6
mruax <matvey.chernyakov@yandex.ru>
2023-10-04 18:56:03 +0500

    Updated logic: - more pawn possible turns

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ class MainWindow(QMainWindow):
         icon = QIcon(str(Path("src/chess.ico")))
         self.setWindowIcon(icon)
 
-        # manually added fixed restriction
+        # manually added fixed headers size restriction
         self.ui.field.horizontalHeader().setMinimumSectionSize(68)
         self.ui.field.horizontalHeader().setMaximumSectionSize(68)
         self.ui.field.verticalHeader().setMinimumSectionSize(68)
@@ -35,10 +35,15 @@ class MainWindow(QMainWindow):
             row = item.row()
             col = item.column()
             print(f"Выбрана ячейка в строке {row + 1}, столбце {col + 1}")
-            if field[row][col]:
-                cells = field[row][col].possible_moves()
+            if self.ui.field.item(row, col).background().color() == QColor(240, 240, 0):  # free cell move
+                print("Тут возможно будет ход!")
+            elif self.ui.field.item(row, col).background().color() == QColor(240, 80, 0):  # attack move
+                print("Возможно тут будет труп!")
+            elif field[row][col]:  # show figure possible moves
+                # cells = field[row][col].possible_moves()
+                cells = pawn_moves(row, col)
                 print("Возможные ходы:", cells)
-                show_possible_items(cells, self.ui.field)
+                show_possible_items(cells, self.ui.field, field[row][col].color)
             else:
                 update_cells(field, self.ui.field)
 
@@ -46,12 +51,41 @@ class MainWindow(QMainWindow):
         print("Новый ход!")
 
 
-def show_possible_items(cells, table):
+def pawn_moves(x, y):
+    figure = field[x][y]
+    c = [1, 2]
+    res = []
+    if figure.color == 0:  # if black - moving down
+        c = [-1, -2]  # coefficients to move up or down
+    if not(field[x - c[0]][y]):  # if next upper cell isn't blocked
+        res.append([x - c[0], y])
+        if figure.first_move:
+            if not(field[x - c[1]][y]):  # if cell through one isn't blocked
+                res.append([x - c[1], y])
+    if y != 0:  # not on left corner
+        t = field[x - c[0]][y - 1]
+        if t:  # if left diagonal cell has figure
+            if t.color != figure.color:
+                res.append([x - c[0], y - 1])  # possible attack cell
+    if y != 7:  # not on right corner
+        t = field[x - c[0]][y + 1]
+        if t:  # if right diagonal cell has figure
+            if t.color != figure.color:
+                res.append([x - c[0], y + 1])  # possible attack cell
+    return res
+
+
+def show_possible_items(cells, table, color):
     update_cells(field, table)
     for cell in cells:
-        # create_figure(cell[0], cell[1], table, QColor(200, 200, 0), pawn_pixmap)
-        # table.clearSelection()
-        create_figure(cell[1], cell[0], table, QPixmap, QColor(240, 240, 0))
+        check_cell = field[cell[0]][cell[1]]
+        if check_cell:  # If figure in cell
+            if check_cell.color == color:  # and same color
+                continue  # No move
+            else:  # possible attack move
+                create_figure(cell[0], cell[1], table, check_cell.image, QColor(240, 80, 0))
+        else:  # free cell move
+            create_figure(cell[0], cell[1], table, QPixmap, QColor(240, 240, 0))
 
 
 def update_cells(matrix, table):
@@ -119,8 +153,16 @@ if __name__ == '__main__':
         print(row)
     start_positions(window.ui.field)
 
+    # test figures:
+    field[5][3] = Pawn(3, 5, color=1, image=white_pawn_pixmap)
+    create_figure(5, 3, window.ui.field, white_pawn_pixmap, default_color(3, 5))
+    field[4][2] = Pawn(2, 4, color=0, image=pawn_pixmap)
+    create_figure(4, 2, window.ui.field, pawn_pixmap, default_color(2, 4))
+    field[4][4] = Pawn(4, 4, color=0, image=pawn_pixmap)
+    create_figure(4, 4, window.ui.field, pawn_pixmap, default_color(4, 4))
+
     current_turn = 1  # 1 - White, 0 - Black
 
-
+    # table.clearSelection()
     window.show()
     sys.exit(app.exec())
